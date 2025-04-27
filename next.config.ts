addc68a2a302f9f9f767a5f5e8a6f16f3f980320
Yoshimi <yoshimi_seo@ginga-sys.jp>
2025-04-27 14:55:39 +0900

    ファイル構成を修正（空のappフォルダを削除）

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
 };
-
-// module.exports = {
-//   output: 'export',
-// };
 
 export default nextConfig;
